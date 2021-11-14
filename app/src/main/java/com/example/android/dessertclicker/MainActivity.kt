3c01b9745c361af7f05641f0220664548f081cac
Masa222a <guitarstring0716@gmail.com>
2021-11-14 22:43:11 +0900

    Android Kotlin Fundamentals: Complex Lifecycle Situations

--- app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -27,10 +27,16 @@ import androidx.databinding.DataBindingUtil
 import com.example.android.dessertclicker.databinding.ActivityMainBinding
 import timber.log.Timber
 
+const val KEY_REVENUE = "revenue_key"
+const val KEY_DESSERT_SOLD = "dessert_sold_key"
+const val KEY_TIMER_SECONDS = "timer_seconds_key"
+
 class MainActivity : AppCompatActivity() {
 
     private var revenue = 0
     private var dessertsSold = 0
+
+    private lateinit var dessertTimer: DessertTimer
 
     // Contains all the views
     private lateinit var binding: ActivityMainBinding
@@ -46,19 +52,19 @@ class MainActivity : AppCompatActivity() {
 
     // Create a list of all desserts, in order of when they start being produced
     private val allDesserts = listOf(
-            Dessert(R.drawable.cupcake, 5, 0),
-            Dessert(R.drawable.donut, 10, 5),
-            Dessert(R.drawable.eclair, 15, 20),
-            Dessert(R.drawable.froyo, 30, 50),
-            Dessert(R.drawable.gingerbread, 50, 100),
-            Dessert(R.drawable.honeycomb, 100, 200),
-            Dessert(R.drawable.icecreamsandwich, 500, 500),
-            Dessert(R.drawable.jellybean, 1000, 1000),
-            Dessert(R.drawable.kitkat, 2000, 2000),
-            Dessert(R.drawable.lollipop, 3000, 4000),
-            Dessert(R.drawable.marshmallow, 4000, 8000),
-            Dessert(R.drawable.nougat, 5000, 16000),
-            Dessert(R.drawable.oreo, 6000, 20000)
+        Dessert(R.drawable.cupcake, 5, 0),
+        Dessert(R.drawable.donut, 10, 5),
+        Dessert(R.drawable.eclair, 15, 20),
+        Dessert(R.drawable.froyo, 30, 50),
+        Dessert(R.drawable.gingerbread, 50, 100),
+        Dessert(R.drawable.honeycomb, 100, 200),
+        Dessert(R.drawable.icecreamsandwich, 500, 500),
+        Dessert(R.drawable.jellybean, 1000, 1000),
+        Dessert(R.drawable.kitkat, 2000, 2000),
+        Dessert(R.drawable.lollipop, 3000, 4000),
+        Dessert(R.drawable.marshmallow, 4000, 8000),
+        Dessert(R.drawable.nougat, 5000, 16000),
+        Dessert(R.drawable.oreo, 6000, 20000)
     )
     private var currentDessert = allDesserts[0]
 
@@ -72,6 +78,15 @@ class MainActivity : AppCompatActivity() {
             onDessertClicked()
         }
 
+        dessertTimer = DessertTimer(this.lifecycle)
+
+        if (savedInstanceState != null) {
+            revenue = savedInstanceState.getInt(KEY_REVENUE, 0)
+            dessertsSold = savedInstanceState.getInt(KEY_DESSERT_SOLD, 0)
+            dessertTimer.secondsCount = savedInstanceState.getInt(KEY_TIMER_SECONDS, 0)
+            showCurrentDessert()
+        }
+
         // Set the TextViews to the right values
         binding.revenue = revenue
         binding.amountSold = dessertsSold
@@ -80,101 +95,115 @@ class MainActivity : AppCompatActivity() {
         binding.dessertButton.setImageResource(currentDessert.imageId)
     }
 
-    /**
-     * Updates the score when the dessert is clicked. Possibly shows a new dessert.
-     */
-    private fun onDessertClicked() {
+        /**
+         * Updates the score when the dessert is clicked. Possibly shows a new dessert.
+         */
+        private fun onDessertClicked() {
 
-        // Update the score
-        revenue += currentDessert.price
-        dessertsSold++
+            // Update the score
+            revenue += currentDessert.price
+            dessertsSold++
 
-        binding.revenue = revenue
-        binding.amountSold = dessertsSold
+            binding.revenue = revenue
+            binding.amountSold = dessertsSold
 
-        // Show the next dessert
-        showCurrentDessert()
-    }
+            // Show the next dessert
+            showCurrentDessert()
+        }
 
-    /**
-     * Determine which dessert to show.
-     */
-    private fun showCurrentDessert() {
-        var newDessert = allDesserts[0]
-        for (dessert in allDesserts) {
-            if (dessertsSold >= dessert.startProductionAmount) {
-                newDessert = dessert
+        /**
+         * Determine which dessert to show.
+         */
+        private fun showCurrentDessert() {
+            var newDessert = allDesserts[0]
+            for (dessert in allDesserts) {
+                if (dessertsSold >= dessert.startProductionAmount) {
+                    newDessert = dessert
+                }
+                // The list of desserts is sorted by startProductionAmount. As you sell more desserts,
+                // you'll start producing more expensive desserts as determined by startProductionAmount
+                // We know to break as soon as we see a dessert who's "startProductionAmount" is greater
+                // than the amount sold.
+                else break
             }
-            // The list of desserts is sorted by startProductionAmount. As you sell more desserts,
-            // you'll start producing more expensive desserts as determined by startProductionAmount
-            // We know to break as soon as we see a dessert who's "startProductionAmount" is greater
-            // than the amount sold.
-            else break
+
+            // If the new dessert is actually different than the current dessert, update the image
+            if (newDessert != currentDessert) {
+                currentDessert = newDessert
+                binding.dessertButton.setImageResource(newDessert.imageId)
+            }
         }
 
-        // If the new dessert is actually different than the current dessert, update the image
-        if (newDessert != currentDessert) {
-            currentDessert = newDessert
-            binding.dessertButton.setImageResource(newDessert.imageId)
-        }
-    }
-
-    /**
-     * Menu methods
-     */
-    private fun onShare() {
-        val shareIntent = ShareCompat.IntentBuilder.from(this)
+        /**
+         * Menu methods
+         */
+        private fun onShare() {
+            val shareIntent = ShareCompat.IntentBuilder.from(this)
                 .setText(getString(R.string.share_text, dessertsSold, revenue))
                 .setType("text/plain")
                 .intent
-        try {
-            startActivity(shareIntent)
-        } catch (ex: ActivityNotFoundException) {
-            Toast.makeText(this, getString(R.string.sharing_not_available),
-                    Toast.LENGTH_LONG).show()
+            try {
+                startActivity(shareIntent)
+            } catch (ex: ActivityNotFoundException) {
+                Toast.makeText(
+                    this, getString(R.string.sharing_not_available),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
+        override fun onCreateOptionsMenu(menu: Menu): Boolean {
+            menuInflater.inflate(R.menu.main_menu, menu)
+            return super.onCreateOptionsMenu(menu)
+        }
+
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            when (item.itemId) {
+                R.id.shareMenuButton -> onShare()
+            }
+            return super.onOptionsItemSelected(item)
+        }
+
+        override fun onSaveInstanceState(outState: Bundle) {
+            super.onSaveInstanceState(outState)
+            Timber.i("onSaveInstanceState Called")
+            outState.putInt(KEY_REVENUE, revenue)
+            outState.putInt(KEY_DESSERT_SOLD, dessertsSold)
+            outState.putInt(KEY_TIMER_SECONDS, dessertTimer.secondsCount)
+        }
+
+        override fun onStart() {
+            super.onStart()
+            dessertTimer.startTimer()
+
+            Timber.i("onCreate called")
+        }
+
+        override fun onResume() {
+            super.onResume()
+            Timber.i("onResume Called")
+        }
+
+        override fun onPause() {
+            super.onPause()
+            Timber.i("onPause Called")
+        }
+
+        override fun onStop() {
+            super.onStop()
+            dessertTimer.stopTimer()
+
+            Timber.i("onStop Called")
+        }
+
+        override fun onDestroy() {
+            super.onDestroy()
+            Timber.i("onDestroy Called")
+        }
+
+        override fun onRestart() {
+            super.onRestart()
+            Timber.i("onRestart Called")
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.shareMenuButton -> onShare()
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Timber.i("onCreate called")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Timber.i("onResume Called")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Timber.i("onPause Called")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Timber.i("onStop Called")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Timber.i("onDestroy Called")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Timber.i("onRestart Called")
-    }
-
-}
